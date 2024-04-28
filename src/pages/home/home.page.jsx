295e6eafc72dbe5ca1974513f016dd7d80cd04c1
bethmij <bethmij@gmail.com>
2024-04-28 18:50:43 +0530

    modify home page

--- src/pages/home/home.page.jsx
+++ src/pages/home/home.page.jsx
@@ -4,19 +4,26 @@ import NavigationBar from "../../components/navigationBar.jsx";
 import {LiaUserCircle} from "react-icons/lia";
 
 
+
+
 function HomePage() {
     return (
-        <div className=" h-screen w-full overflow-y-hidden">
-            {/*<div id="homePage" className=" h-screen w-full opacity-30 static">*/}
+        <div className="h-screen w-full ">
+            <div id="homePage" className=" h-screen w-full opacity-30 absolute"></div>
 
-            <div id="mainPic" className="w-2/5 h-2/5 bg-amber-300 mt-5 float-left"></div>
-            <div className=" w-3/5  h-20 flex ps-80 mt-36">
+            <div id="mainPic" className="w-2/5 h-2/5 bg-amber-300  float-left"></div>
+            <div className=" w-3/5  h-20 flex ps-80 pt-52 ">
                 <NavigationBar className="self-center"/>
-                <LiaUserCircle color="#D4BACE" size="80" className="ms-2 self-center"/>
+                <LiaUserCircle color="#D4BACE" size="80" className="ms-2 self-center mb-20"/>
             </div>
 
-            {/*<Link className="text-amber-800" to={"/page"}>Page</Link>*/}
-            {/*</div>*/}
+            <div
+                className="w-2/3 h-2/5 bg-transparent ms-80 border-t-4 border-b-4 opacity-90 flex flex-col justify-center items-center" color= "#130D13">
+                <h1 className="text-7xl">EVERYDAY COMFORT</h1>
+                <p className=" text-3xl text-center mt-10">Step into style and step into confidence <br/> with our curated collection of
+                    footwear.</p>
+            </div>
+
 
         </div>
     )
