dfd526bfa6aa0f88b4431cdea72ee35fec7af4d9
bethmij <bethmij@gmail.com>
2024-04-28 14:02:19 +0530

    add home page

---
+++ src/pages/home/home.page.jsx
@@ -0,0 +1,22 @@
+// import {Link} from "react-router-dom";
+
+import {Icon} from "lucide-react";
+
+function HomePage() {
+    return (
+        <div className="bg-black h-screen w-full">
+            {/*<div id="homePage" className=" h-screen w-full opacity-30 static">*/}
+            <div>
+                <div className=" w-1/5 h-1/5 bg-amber-300"></div>
+                <img src="../../assets/Flower%20Shoes%20Logo.png"/>
+            </div>
+
+            {/*<Link className="text-amber-800" to={"/page"}>Page</Link>*/}
+            {/*</div>*/}
+
+        </div>
+    )
+
+}
+
+export default HomePage
