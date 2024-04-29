bcec9db6ef478154d7955b100ec95022f177a2e5
bethmij <bethmij@gmail.com>
2024-04-29 11:37:17 +0530

    update form input rootLayout

---
+++ src/components/shared/input.jsx
@@ -0,0 +1,19 @@
+
+// import {Input} from "../ui/input.jsx"
+
+
+export function InputItem() {
+
+
+    return (
+        <>
+            {/*<label>Username</label>*/}
+            {/*<Input type="email" placeholder="Email"/>*/}
+            <p>
+                This is your public display name.
+            </p>
+        </>
+
+
+    )
+}
