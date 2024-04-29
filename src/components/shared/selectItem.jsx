ef5aed2b820ee5b9bed62c4b65325843da2f1e25
bethmij <bethmij@gmail.com>
2024-04-29 16:40:22 +0530

    update customer form

--- src/components/shared/selectItem.jsx
+++ src/components/shared/selectItem.jsx
@@ -1,31 +1,28 @@
-// import {
-//     Select,
-//     SelectContent,
-//     SelectGroup,
-//     SelectItem,
-//     SelectLabel,
-//     SelectTrigger,
-//     SelectValue,
-// } from "@/components/ui/select"
+import {
+    Select,
+    SelectContent,
+    SelectGroup,
+    SelectItem,
+    SelectTrigger,
+    SelectValue,
+} from "@/components/ui/select"
 import PropTypes from "prop-types";
 
 export function SelectItems(props) {
-    console.log(props)
-    // return (
-    //     <Select className="z-10 mt-10">
-    //         <SelectTrigger className="w-full mt-2">
-    //             <SelectValue placeholder={props.placeholder} />
-    //         </SelectTrigger>
-    //         <SelectContent>
-    //             <SelectGroup>
-    //                 <SelectLabel>{props.title}</SelectLabel>
-    //                 {props.list.map( item => {
-    //                     return <SelectItem key={item} value={item}>{item}</SelectItem>
-    //                 })}
-    //             </SelectGroup>
-    //         </SelectContent>
-    //     </Select>
-    // )
+    return (
+        <Select className="z-10 mt-10">
+            <SelectTrigger className="w-full mt-2 mb-2">
+                <SelectValue placeholder={props.placeholder} />
+            </SelectTrigger>
+            <SelectContent>
+                <SelectGroup>
+                    {props.list.map( item => {
+                        return <SelectItem key={item} value={item}>{item}</SelectItem>
+                    })}
+                </SelectGroup>
+            </SelectContent>
+        </Select>
+    )
 }
 
 SelectItems.propTypes = {
