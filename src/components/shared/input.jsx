22fe54c822154499f2f2c05c565d307c89ef2849
bethmij <bethmij@gmail.com>
2024-04-29 13:28:20 +0530

    update form rootLayout

--- src/components/shared/input.jsx
+++ src/components/shared/input.jsx
@@ -1,19 +1,27 @@
+import {Input} from "../ui/input.jsx"
+import PropTypes from "prop-types";
 
-// import {Input} from "../ui/input.jsx"
 
-
-export function InputItem() {
+export function InputItem(props) {
 
 
     return (
-        <>
-            {/*<label>Username</label>*/}
-            {/*<Input type="email" placeholder="Email"/>*/}
-            <p>
-                This is your public display name.
+
+        <div className="z-10 ms-16 mt-10 w-1/3" key={props.title}>
+            <label className="text-l">{props.title}</label>
+            <Input type={props.type} placeholder={props.placeholder} className="mt-2 mb-2 w-full "/>
+            <p className="opacity-60">
+                {props.description}
             </p>
-        </>
+        </div>
 
 
     )
 }
+
+InputItem.propTypes = {
+    title: PropTypes.string.isRequired,
+    description: PropTypes.string,
+    placeholder: PropTypes.string,
+    type: PropTypes.string.isRequired
+};
