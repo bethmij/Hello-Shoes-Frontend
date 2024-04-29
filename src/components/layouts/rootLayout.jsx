458b5ae50de1eb6ace916c85324423aaf2fa6caf
bethmij <bethmij@gmail.com>
2024-04-29 16:40:12 +0530

    update customer form

--- src/components/layouts/rootLayout.jsx
+++ src/components/layouts/rootLayout.jsx
@@ -9,6 +9,7 @@ import {CgFormatRight} from "react-icons/cg";
 import CurrentTime from "../shared/time.jsx";
 import {InputItem} from "../shared/input.jsx";
 import {ScrollArea} from "@/components/ui/scroll-area";
+import {Button} from "../ui/button.jsx";
 
 
 function RootLayout() {
@@ -76,54 +77,6 @@ function RootLayout() {
                 description: "Full name required",
             }],
         [{
-            title: "Address Line 01",
-            type: "text",
-            placeholder: "Address",
-            description: "Building no or name",
-        },
-            {
-                title: "Address Line 02",
-                type: "text",
-                placeholder: "Address",
-                description: "Lane",
-            }],
-        [{
-            title: "Address Line 03",
-            type: "text",
-            placeholder: "Address",
-            description: "Main city",
-        },
-            {
-                title: "Address Line 04",
-                type: "text",
-                placeholder: "Address",
-                description: "Main state",
-            }],
-        [{
-            title: "Address Line 05",
-            type: "text",
-            placeholder: "Address",
-            description: "Postal code",
-        },
-            {
-                title: "Contact number",
-                type: "text",
-                placeholder: "Contact",
-                description: "Mobile number",
-            }],
-        [{
-            title: "Customer Email",
-            type: "email",
-            placeholder: "Email",
-            description: "Email required",
-        },
-            {
-                title: "Total point",
-                type: "number",
-                placeholder: "Points",
-                description: "1 point purchase more than LKR 800",
-            }],
-        [{
             title: "Customer Gender",
             type: "select",
             placeholder: "Gender",
@@ -131,25 +84,75 @@ function RootLayout() {
             selectList: ["Male", "Female", "Other"]
         },
             {
-                title: "Customer Level",
-                type: "select",
-                placeholder: "Level",
-                description: "Gold > 200  | Silver – 100-199 | Bronze – 50-99 | New < 50",
-                selectList: ["Gold", "Silver", "Bronze","New"]
+                title: "Loyalty Join Date",
+                type: "date",
+                placeholder: "Date",
+                description: "Date of the entitlement as a loyalty customer",
             }],
         [{
-            title: "Customer DOB",
-            type: "date",
-            placeholder: "Date",
-            description: "Date of birth",
+            title: "Total point",
+            type: "number",
+            placeholder: "Points",
+            description: "1 point purchase more than LKR 800",
         },
             {
                 title: "Customer Level",
                 type: "select",
                 placeholder: "Level",
                 description: "Gold > 200  | Silver – 100-199 | Bronze – 50-99 | New < 50",
-                selectList: ["Gold", "Silver", "Bronze","New"]
+                selectList: ["Gold", "Silver", "Bronze", "New"]
+            }],
+        [
+            {
+                title: "Customer DOB",
+                type: "date",
+                placeholder: "Date",
+                description: "Date of birth",
+            },
+            {
+                title: "Address Line 01",
+                type: "text",
+                placeholder: "Address",
+                description: "Building no or name",
+            }], [
+            {
+                title: "Address Line 02",
+                type: "text",
+                placeholder: "Address",
+                description: "Lane",
+            },
+            {
+                title: "Address Line 03",
+                type: "text",
+                placeholder: "Address",
+                description: "Main city",
+            }],
+        [{
+            title: "Address Line 04",
+            type: "text",
+            placeholder: "Address",
+            description: "Main state",
+        },
+            {
+                title: "Address Line 05",
+                type: "text",
+                placeholder: "Address",
+                description: "Postal code",
+            }], [
+            {
+                title: "Contact number",
+                type: "text",
+                placeholder: "Contact",
+                description: "Mobile number",
+            },
+            {
+                title: "Customer Email",
+                type: "email",
+                placeholder: "Email",
+                description: "Email required",
             }]
+
+
     ]
 
 
@@ -187,25 +190,28 @@ function RootLayout() {
                     </div>
                 </nav>
 
-                <div className="w-4/5 h-4/5 ms-52 mt-32 flex-col  rounded-3xl absolute xl ">
-                    <ScrollArea className="h-full w-full rounded-3xl ">
-                        <div id="form" className="w-full h-full absolute border-2  rounded-3xl "></div>
+                <div className="w-4/5 h-4/5 ms-52 mt-32 flex-col rounded-3xl absolute xl">
+                    <ScrollArea className="h-full w-full rounded-3xl z-0">
+                        <div id="form" className="w-full h-full absolute border-2 z-0 rounded-3xl "></div>
 
-                        {customerForm.map(customer => {
-                            return <div key={customerForm.indexOf(customer)} className="flex justify-around mb-4 z-10">
-                                {customer.map(data => {
-                                    return <InputItem key={data.title} title={data.title}
-                                                      placeholder={data.placeholder}
-                                                      type={data.type} description={data.description}
-                                                      selectList={data.selectList}/>
 
-                                })}
+                        {customerForm.map((customer, index) => (
+                            <div key={index} className="flex justify-around mb-4 z-10">
+                                {customer.map(data => (
+                                    <InputItem
+                                        key={data.title}
+                                        title={data.title}
+                                        placeholder={data.placeholder}
+                                        type={data.type}
+                                        description={data.description}
+                                        selectList={data.selectList}
+                                    />
+                                ))}
                             </div>
-                        })}
-
-                        {/*<SelectItems/>*/}
-
+                        ))}
                     </ScrollArea>
+
+                    <Button className=" w-2/12 h-12 absolute text-2xl text-opacity-40 -bottom-10 right-0 m-5 mt-10">Submit</Button>
                 </div>
 
 
