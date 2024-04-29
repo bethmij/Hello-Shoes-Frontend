9acc8b697e9162e82787b8497e92ac97b63bb680
bethmij <bethmij@gmail.com>
2024-04-29 10:18:59 +0530

    update  root layout

--- src/components/layouts/rootLayout.jsx
+++ src/components/layouts/rootLayout.jsx
@@ -5,39 +5,39 @@ import {CgProfile} from "react-icons/cg";
 import {BsPersonDown, BsPersonBadge} from "react-icons/bs";
 import {GoPackageDependencies} from "react-icons/go";
 import {VscGraph} from "react-icons/vsc";
-// import {useState} from "react";
-
+import {CgFormatRight} from "react-icons/cg";
+import CurrentTime from "../shared/time.jsx";
 
 function RootLayout() {
 
     const menuItem = [
         {
             to: "/page",
-            icon: <CiHome size="55"/>
+            icon: <CiHome size="55" color="#78045B"/>
         },
         {
             to: "/sale",
-            icon: <PiShoppingCartLight size="55"/>
+            icon: <PiShoppingCartLight size="55" color="#78045B"/>
         },
         {
             to: "/inventory",
-            icon: <PiHighHeelThin size="55"/>
+            icon: <PiHighHeelThin size="55" color="#78045B"/>
         },
         {
             to: "/customer",
-            icon: <BsPersonDown size="55"/>
+            icon: <BsPersonDown size="55" color="#78045B"/>
         },
         {
             to: "/supplier",
-            icon: <GoPackageDependencies size="55"/>
+            icon: <GoPackageDependencies size="55" color="#78045B"/>
         },
         {
             to: "/employee",
-            icon: <BsPersonBadge size="55"/>
+            icon: <BsPersonBadge size="55" color="#78045B"/>
         },
         {
             to: "/admin-panel",
-            icon: <VscGraph size="55"/>
+            icon: <VscGraph size="55" color="#78045B"/>
         },
         {
             to: "/",
@@ -53,7 +53,7 @@ function RootLayout() {
         },
         {
             to: "/user",
-            icon: <CgProfile size="55"/>
+            icon: <CgProfile size="55" color="#78045B"/>
         }
 
 
@@ -61,25 +61,45 @@ function RootLayout() {
 
     // const [isClicked, setIsClicked] = useState(false)
 
+
     return (
         <>
-
             <div id="root" className="h-screen w-full absolute"></div>
-            <div className="h-screen w-24 flex  justify-center items-center">
-                <div className="h-screen w-24 flex flex-col justify-around items-center relative">
+            <div className="w-full h-20 absolute bg-black opacity-50"></div>
 
-                    {menuItem.map(item => {
-                        return <Link key={item.to} className="no-underline" to={item.to} style={{zIndex: "1"}}>
-                            <div className={`mt-4 bg-bermuda cursor-pointer `}>
-                                {item.icon}
-                            </div>
-                        </Link>
-                    })}
-
-
-                    <div className="h-screen w-20 bg-purple opacity-30 absolute"></div>
-
+            <div className=" w-full h-20 absolute flex justify-between">
+                <div id="nameTag" className="h-full w-60 ms-28 mt-3"></div>
+                <div className="self-center flex gap-x-3">
+                    <CgFormatRight size="45"/>
+                    <h1 className="text-4xl">Customer Form</h1>
                 </div>
+                <div className="me-5">
+                    <CurrentTime/>
+                </div>
+            </div>
+
+            <div className="flex">
+
+                <nav className="h-screen w-24 flex  justify-center items-center">
+                    <div className="h-screen w-24 flex flex-col justify-around items-center relative">
+                        {menuItem.map(item => {
+                            return <Link key={item.to} className="no-underline" to={item.to} style={{zIndex: "1"}}>
+                                <div className={`mt-4 cursor-pointer `}>
+                                    {item.icon}
+                                </div>
+                            </Link>
+                        })}
+                        <div className="h-screen w-20 bg-purple opacity-40 absolute"></div>
+                    </div>
+                </nav>
+
+                <div  className="w-4/5 h-4/5 ms-48 mt-32 bg-transparent flex justify-center items-center absolute">
+                    <div id="form" className="w-full h-full bg-purple backdrop-blur-3xl opacity-40 absolute"></div>
+                    {/*<h1 className="text-4xl text-white">Content Here</h1>*/}
+                </div>
+
+
+
             </div>
 
 
