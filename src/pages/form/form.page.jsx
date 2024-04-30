3c37e34ac4d920652a5a206856c8c20a2d8bff73
bethmij <bethmij@gmail.com>
2024-04-30 15:47:15 +0530

    employee form page add

--- src/pages/form/form.page.jsx
+++ src/pages/form/form.page.jsx
@@ -4,46 +4,66 @@ import {useForm} from "react-hook-form";
 import {ScrollArea} from "../../components/ui/scroll-area.jsx";
 import {useParams} from "react-router-dom";
 import {customerForm} from "./formDetail/customer.js";
+import {employeeForm} from "./formDetail/employee.js";
+import {CgFormatRight} from "react-icons/cg";
 
 
 function FormPage() {
 
-    const {id} =useParams()
+    const {id} = useParams()
+    let form = ""
+    let title = ""
+
+    if(id === "customer"){
+        form = customerForm
+        title = "Customer Form"
+    }else if(id === "employee"){
+        form = employeeForm
+        title = "Employee Form"
+    }
 
 
     const {register, handleSubmit} = useForm()
 
     return (
-        <form className="w-4/5 h-4/5 ms-52 mt-32 flex-col rounded-3xl absolute xl"
-              onSubmit={handleSubmit(data => {
-                  console.log(data)
-              })}>
-            <ScrollArea className="h-full w-full rounded-3xl z-0">
-                <div id="form" className="w-full h-full absolute border-2 z-0 rounded-3xl "></div>
+        <>
+            <div className="absolute top-0 left-1/2 flex gap-x-5 -ms-32 mt-4 opacity-80">
+                <CgFormatRight size="45"/>
+                <h1 className="text-4xl ">{title}</h1>
+            </div>
 
 
-                {customerForm.map((customer, index) => (
-                    <div key={index} className="flex justify-around mb-4 z-10">
-                        {customer.map(data => (
-                            <InputItem
-                                key={data.id}
-                                id={data.id}
-                                title={data.title}
-                                placeholder={data.placeholder}
-                                type={data.type}
-                                description={data.description}
-                                selectList={data.selectList}
-                                register={register}
-                            />
+            <form className="w-4/5 h-4/5 ms-52 mt-32 flex-col rounded-3xl absolute xl"
+                  onSubmit={handleSubmit(data => {
+                      console.log(data)
+                  })}>
+                <ScrollArea className="h-full w-full rounded-3xl z-0">
+                    <div id="form" className="w-full h-full absolute border-2 z-0 rounded-3xl "></div>
 
-                        ))}
-                    </div>
-                ))}
-            </ScrollArea>
+                    {form.map((formData, index) => (
+                        <div key={index} className="flex justify-around mb-4 z-10">
+                            {formData.map(data => (
+                                <InputItem
+                                    key={data.id}
+                                    id={data.id}
+                                    title={data.title}
+                                    placeholder={data.placeholder}
+                                    type={data.type}
+                                    description={data.description}
+                                    selectList={data.selectList}
+                                    register={register}
+                                />
 
-            <Button type="submit"
-                    className=" w-2/12 h-12 absolute text-2xl text-opacity-40 -bottom-10 right-0 m-5 mt-10">Submit</Button>
-        </form>
+                            ))}
+                        </div>
+                    ))
+            }
+                </ScrollArea>
+
+                <Button type="submit"
+                        className=" w-2/12 h-12 absolute text-2xl text-opacity-40 -bottom-10 right-0 m-5 mt-10">Submit</Button>
+            </form>
+        </>
     )
 
 }
---
+++ src/pages/form/formDetail/employee.js
@@ -0,0 +1,138 @@
+const genderList = ["Male", "Female", "Other"]
+const statusList = ["Married", "Single"]
+const accessRoleList = ["Admin", "User"]
+
+export const employeeForm = [
+    [{
+        id: "code",
+        title: "Employee Code",
+        type: "text",
+        placeholder: "",
+        description: "Auto generated",
+    },
+        {
+            id: "name",
+            title: "Employee Name",
+            type: "text",
+            placeholder: "Name",
+            description: "Full name required",
+        }],
+    [{
+        id: "gender",
+        title: "Employee Gender",
+        type: "select",
+        placeholder: "Gender",
+        description: "",
+        selectList: genderList
+    },
+        {
+            id: "status",
+            title: "Employee Status",
+            type: "select",
+            placeholder: "Status",
+            description: "",
+            selectList: statusList
+        }],
+    [{
+        id: "role",
+        title: "Access Role",
+        type: "text",
+        placeholder: "Role",
+        description: "",
+        selectList: accessRoleList
+    },
+        {
+            id: "designation",
+            title: "Designation",
+            type: "text",
+            placeholder: "Designation",
+            description: "Designation of employee",
+        }],
+    [
+        {
+            id: "dob",
+            title: "Employee DOB",
+            type: "date",
+            placeholder: "Date",
+            description: "Date of birth",
+        },
+        {
+            id: "joinDate",
+            title: "Join Date",
+            type: "date",
+            placeholder: "Date",
+            description: "Date of Joined to the company",
+        }],
+        [{
+            id: "branch",
+            title: "Attached Branch",
+            type: "text",
+            placeholder: "Branch",
+            description: "Which branch attached",
+        }, {
+            id: "address1",
+            title: "Address Line 01",
+            type: "text",
+            placeholder: "Address",
+            description: "Building no or name",
+        }], [
+        {
+            id: "address2",
+            title: "Address Line 02",
+            type: "text",
+            placeholder: "Address",
+            description: "Lane",
+        },
+        {
+            id: "address3",
+            title: "Address Line 03",
+            type: "text",
+            placeholder: "Address",
+            description: "Main city",
+        }],
+        [{
+            id: "address4",
+            title: "Address Line 04",
+            type: "text",
+            placeholder: "Address",
+            description: "Main state",
+        },
+        {
+            id: "address5",
+            title: "Address Line 05",
+            type: "text",
+            placeholder: "Address",
+            description: "Postal code",
+        }], [
+        {
+            id: "contact",
+            title: "Contact number",
+            type: "text",
+            placeholder: "Contact",
+            description: "Mobile number",
+        },
+        {
+            id: "email",
+            title: "Customer Email",
+            type: "email",
+            placeholder: "Email",
+            description: "Email required",
+        }],
+    [
+        {
+            id: "guardian",
+            title: "Guardian",
+            type: "text",
+            placeholder: "Guardian",
+            description: "Name the guardian or nominated person",
+        },
+        {
+            id: "emergencyContact",
+            title: "Emergency Contact",
+            type: "text",
+            placeholder: "Contact",
+            description: "Inform in case of emergency",
+        }]
+
+
+]
