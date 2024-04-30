bea1115f18f85b090ef9322a94247e89a6bca66b
bethmij <bethmij@gmail.com>
2024-04-30 14:41:45 +0530

    updated customer form

--- src/components/shared/selectItem.jsx
+++ src/components/shared/selectItem.jsx
@@ -10,18 +10,19 @@ import PropTypes from "prop-types";
 
 export function SelectItems(props) {
     return (
-        <Select className="z-10 mt-10">
+        <Select  className="z-10 mt-10"> {/* Forward ref to the Select component */}
             <SelectTrigger className="w-full mt-2 mb-2">
                 <SelectValue placeholder={props.placeholder} />
             </SelectTrigger>
             <SelectContent>
                 <SelectGroup>
-                    {props.list.map( item => {
+                    {props.list.map(item => {
                         return <SelectItem key={item} value={item}>{item}</SelectItem>
                     })}
                 </SelectGroup>
             </SelectContent>
         </Select>
+
     )
 }
 
