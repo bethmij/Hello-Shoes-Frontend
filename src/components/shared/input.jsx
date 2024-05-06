66e7c061b21f8ec644131ee557ae79939455c3b8
bethmij <bethmij@gmail.com>
2024-05-06 14:21:16 +0530

    input component updated

--- src/components/shared/input.jsx
+++ src/components/shared/input.jsx
@@ -14,18 +14,25 @@ export function InputItem(props) {
                     name={props.id}
                     placeholder={props.placeholder}
                     className="mt-2 mb-2 w-full"
+                    value={(props.value !== null ) ? props.value : props.placeholder}
+                    readOnly={props.isEdit}
                 />
             );
         } else if (props.type === 'select') {
             return (
 
                 <select {...props.register(props.id)} name={props.id} id={props.id}
+                        onChange={(event) => {
+                            const selectedOption = event.target.value;
+                            props.onChange(selectedOption);
+                        }}
                         className="mt-2 mb-2 w-full h-10 border rounded-md border-input bg-background px-3 py-2 text-sm focus:outline-none ">
                     <option> {props.placeholder}</option>
                     {props.selectList.map(item => (
                         <option
                             key={item}
                             value={item}
+
                             className=""
                         >
                             {item}
@@ -37,7 +44,7 @@ export function InputItem(props) {
     }
 
     return (
-        <div className="z-10 ms-16 mt-10 w-1/3" key={props.id}>
+        <div className="z-10 ms-10 mt-10 w-2/5" key={props.id}>
             <label className="text-lg">{props.title}</label>
             {setInput()}
             <p className="opacity-60">
@@ -56,5 +63,8 @@ InputItem.propTypes = {
     selectList: PropTypes.array,
     register: PropTypes.func.isRequired,
     errors: PropTypes.object,
-    watch: PropTypes.func
+    watch: PropTypes.func,
+    onChange: PropTypes.func,
+    value: PropTypes.string,
+    isEdit: PropTypes.bool
 };
