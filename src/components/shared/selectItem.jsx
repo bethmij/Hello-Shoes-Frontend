6be271d02a5cc8bc3ba8f43762931c723ef3c1d3
bethmij <bethmij@gmail.com>
2024-05-18 00:57:16 +0530

    select component updated

--- src/components/shared/selectItem.jsx
+++ src/components/shared/selectItem.jsx
@@ -7,28 +7,35 @@ import {
     SelectValue,
 } from "@/components/ui/select"
 import PropTypes from "prop-types";
+import {Label} from "../ui/label.jsx";
 
 export function SelectItems(props) {
     return (
-        <Select  className="z-10 mt-10"> {/* Forward ref to the Select component */}
-            <SelectTrigger className="w-full mt-2 mb-2">
-                <SelectValue placeholder={props.placeholder} />
-            </SelectTrigger>
-            <SelectContent>
-                <SelectGroup>
+
+        <div className="mt-6">
+            <Label htmlFor={props.id} className="text-xl mb-20 pb-10" >{props.title} </Label>
+            <Select onValueChange={(value) => props.setValue(`${props.id}`, value)} className="bg-transparent">
+                <SelectTrigger >
+                    <SelectValue placeholder={ props.placeholder} />
+                </SelectTrigger>
+                <SelectGroup >
+                    <SelectContent >
                     {props.list.map(item => {
                         return <SelectItem key={item} value={item}>{item}</SelectItem>
                     })}
+                    </SelectContent>
                 </SelectGroup>
-            </SelectContent>
-        </Select>
+            </Select>
+        </div>
 
     )
 }
 
 SelectItems.propTypes = {
+    id:PropTypes.string.isRequired,
     title: PropTypes.string.isRequired,
     placeholder: PropTypes.string,
-    list: PropTypes.array.isRequired
+    list: PropTypes.array.isRequired,
+    setValue:PropTypes.func
 };
 
