eb7c252a42866364f6f3c9ed55497ac425c742be
bethmij <bethmij@gmail.com>
2024-05-19 09:08:17 +0530

    select items updated

--- src/components/shared/selectItem.jsx
+++ src/components/shared/selectItem.jsx
@@ -13,16 +13,20 @@ export function SelectItems(props) {
     return (
 
         <div className="mt-6">
-            <Label htmlFor={props.id} className="text-xl mb-20 pb-10" >{props.title} </Label>
-            <Select onValueChange={(value) => props.setValue(`${props.id}`, value)} className="bg-transparent">
-                <SelectTrigger >
-                    <SelectValue placeholder={ props.placeholder} />
+            <Label htmlFor={props.id} className="text-xl mb-20 pb-10">{props.title} </Label>
+            <Select onValueChange={(value) => {
+                props.setValue(`${props.id}`, value)
+                props.onSubmit(value)
+            }}
+                    className="bg-transparent">
+                <SelectTrigger>
+                    <SelectValue placeholder={props.placeholder}/>
                 </SelectTrigger>
-                <SelectGroup >
-                    <SelectContent >
-                    {props.list.map(item => {
-                        return <SelectItem key={item} value={item}>{item}</SelectItem>
-                    })}
+                <SelectGroup>
+                    <SelectContent>
+                        {props.list.map(item => {
+                            return <SelectItem key={item} value={item}>{item}</SelectItem>
+                        })}
                     </SelectContent>
                 </SelectGroup>
             </Select>
@@ -32,10 +36,11 @@ export function SelectItems(props) {
 }
 
 SelectItems.propTypes = {
-    id:PropTypes.string.isRequired,
+    id: PropTypes.string.isRequired,
     title: PropTypes.string.isRequired,
     placeholder: PropTypes.string,
     list: PropTypes.array.isRequired,
-    setValue:PropTypes.func
+    setValue: PropTypes.func,
+    onSubmit: PropTypes.func
 };
 
