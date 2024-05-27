786478b906bbbf61fabc61ca3b7455f808bd425f
bethmij <bethmij@gmail.com>
2024-05-27 23:58:24 +0530

    cash payment updated

--- src/components/shared/CashPayment.jsx
+++ src/components/shared/CashPayment.jsx
@@ -5,11 +5,12 @@ import {InputItem} from "./input.jsx";
 import {Label} from "../ui/label.jsx";
 import {Input} from "../ui/input.jsx";
 import {Button} from "../ui/button.jsx";
-import React from "react";
+import React, {useState} from "react";
 import PropTypes from "prop-types";
 
 export function CashPayment(props) {
-    const {register} = useForm()
+    const {register, handleSubmit} = useForm()
+    const [cashPrice, setCashPrice] = useState()
     return (
         <>
             <Card>
@@ -19,29 +20,42 @@ export function CashPayment(props) {
                         Add your information below to pay by cash
                     </CardDescription>
                 </CardHeader>
-                <CardContent className="space-y-2 mt-7">
-                    <div className="flex justify-center">
-                        <img src={paymentImg} alt="img" width={200} className="m-0 "/>
-                    </div>
-                    <div className="-ms-10 -mt-20">
-                        <InputItem id={"amount"} title={"Payment Amount"} isEdit={true} type={"text"}
-                                   value={`$ ${props.totalPrice}`} register={register}></InputItem>
+                <form onSubmit={handleSubmit(() => {
+                    if (cashPrice>=props.totalPrice) {
+                        props.isPaid(true)
+                        swal("Success", "Payment Successful!", "success")
+                    }else {
+                        swal("Error", "Insufficient Payment Amount", "error")
+                    }
+                })}>
+                    <CardContent className="space-y-2 mt-7">
 
-                    </div>
-                    <div className="-ms-10 -mt-10">
-                        <InputItem id={"cash"} title={"Card Amount"} type={"number"} placeholder="$ 0.0"
-                                   register={register} onChange={props.onChange}></InputItem>
-                    </div>
-                    <div className="-ms-10 -mt-10 mb-10">
-                        <InputItem id={"balance"} title={"Balance"} type={"number"} placeholder="$ 0.0"
-                                   register={register} isEdit={true} value={props.balance} ></InputItem>
-                    </div>
+                        <div className="flex justify-center">
+                            <img src={paymentImg} alt="img" width={200} className="m-0 "/>
+                        </div>
+                        <div className="-ms-10 -mt-20">
+                            <InputItem id={"amount"} title={"Payment Amount"} isEdit={true} type={"text"}
+                                       value={`$ ${props.totalPrice}`} register={register}></InputItem>
+
+                        </div>
+                        <div className="-ms-10 -mt-10">
+                            <InputItem id={"cash"} title={"Card Amount"} type={"number"} placeholder="$ 0.0"
+                                       register={register} onChange={(event) => {
+                                                props.onChange
+                                                setCashPrice(event.target.value)
+                            }} isRequired={true}></InputItem>
+                        </div>
+                        <div className="-ms-10 -mt-10 mb-10">
+                            <InputItem id={"balance"} title={"Balance"} type={"number"} placeholder="$ 0.0"
+                                       register={register} isEdit={true} value={props.balance}></InputItem>
+                        </div>
 
 
-                </CardContent>
-                <CardFooter>
-                    <Button className="w-full text-lg">Confirm Payment</Button>
-                </CardFooter>
+                    </CardContent>
+                    <CardFooter>
+                        <Button className="w-full text-lg">Confirm Payment</Button>
+                    </CardFooter>
+                </form>
             </Card>
         </>
     )
@@ -51,5 +65,6 @@ export function CashPayment(props) {
 CashPayment.propTypes = {
     totalPrice: PropTypes.number.isRequired,
     balance: PropTypes.number.isRequired,
-    onChange: PropTypes.func.isRequired
+    onChange: PropTypes.func.isRequired,
+    isPaid: PropTypes.func.isRequired
 }
