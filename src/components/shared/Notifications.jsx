8015c0f12214349a74786b4583150689394057a2
bethmij <bethmij@gmail.com>
2024-05-27 23:57:14 +0530

    notification bar updated

--- src/components/shared/Notifications.jsx
+++ src/components/shared/Notifications.jsx
@@ -1,64 +1,42 @@
-import { Button } from "@/components/ui/button"
-import { Input } from "@/components/ui/input"
-import { Label } from "@/components/ui/label"
+import {Button} from "@/components/ui/button"
 import {
     Popover,
     PopoverContent,
     PopoverTrigger,
 } from "@/components/ui/popover"
-import { IoIosNotificationsOutline } from "react-icons/io";
+import {IoIosNotificationsOutline} from "react-icons/io";
+import {AlertBox} from "./AlertBox.jsx";
+import {useEffect, useState} from "react";
+import {getDetails} from "../../pages/cart/cardDetail/fetchData.jsx";
+import {AiOutlineAlert} from "react-icons/ai";
+
+// import { RocketIcon } from "@radix-ui/react-icons"
 
 
 export function Notifications() {
+
+    const [alert, setAlert] = useState([])
+
+    useEffect(() => {
+        getDetails("inventory", "lowStock").then(items => {
+                setAlert(items)
+        })
+    }, []);
     return (
         <>
             <Popover>
                 <PopoverTrigger asChild>
-                    <Button variant="outline" className="border-0"><IoIosNotificationsOutline/></Button>
+                    <Button variant="outline" className="border-0"><IoIosNotificationsOutline size={30}/></Button>
                 </PopoverTrigger>
-                <PopoverContent className="w-80">
-                    <div className="grid gap-4">
-                        <div className="space-y-2">
-                            <h4 className="font-medium leading-none">Dimensions</h4>
-                            <p className="text-sm text-muted-foreground">
-                                Set the dimensions for the layer.
-                            </p>
-                        </div>
-                        <div className="grid gap-2">
-                            <div className="grid grid-cols-3 items-center gap-4">
-                                <Label htmlFor="width">Width</Label>
-                                <Input
-                                    id="width"
-                                    defaultValue="100%"
-                                    className="col-span-2 h-8"
+                <PopoverContent className="w-[30vw] flex flex-col gap-y-8">
+                    {alert.map(item => {
+                        return <AlertBox key={item.itemCode} title="LOW STOCK"
+                                         desc={`Item Code - ${item.itemCode} is having low stocks - Item Qty - ${item.itemQty}`}
+                                            icon={<AiOutlineAlert/>}
                                 />
-                            </div>
-                            <div className="grid grid-cols-3 items-center gap-4">
-                                <Label htmlFor="maxWidth">Max. width</Label>
-                                <Input
-                                    id="maxWidth"
-                                    defaultValue="300px"
-                                    className="col-span-2 h-8"
-                                />
-                            </div>
-                            <div className="grid grid-cols-3 items-center gap-4">
-                                <Label htmlFor="height">Height</Label>
-                                <Input
-                                    id="height"
-                                    defaultValue="25px"
-                                    className="col-span-2 h-8"
-                                />
-                            </div>
-                            <div className="grid grid-cols-3 items-center gap-4">
-                                <Label htmlFor="maxHeight">Max. height</Label>
-                                <Input
-                                    id="maxHeight"
-                                    defaultValue="none"
-                                    className="col-span-2 h-8"
-                                />
-                            </div>
-                        </div>
-                    </div>
+
+                    })}
+
                 </PopoverContent>
             </Popover>
         </>
